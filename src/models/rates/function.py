e339799bc976b91d0aa1765665a033c3c638f9fb
matsagad <matsagad@gmail.com>
2023-01-28 14:12:19 +0000

    Add rate functions

---
+++ src/models/rates/function.py
@@ -0,0 +1,20 @@
+from typing import Callable
+import numpy as np
+
+
+class RateFunction:
+    @staticmethod
+    def constant(a: float) -> Callable[[np.ndarray, np.ndarray], float]:
+        return lambda exo_states: np.repeat(a, exo_states.shape[1])
+
+    @staticmethod
+    def linear(a: float, exo_index: int) -> Callable[[np.ndarray, np.ndarray], float]:
+        return lambda exo_state: a * exo_state[exo_index]
+
+    @staticmethod
+    def hill(
+        a: float, b: float, exo_index: int
+    ) -> Callable[[np.ndarray, np.ndarray], float]:
+        return lambda exo_state: (
+            (a * exo_state[exo_index] / b) / (1 + exo_state[exo_index] / b)
+        )
