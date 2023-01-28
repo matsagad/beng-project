7ca13fc65ae73cb337b06ecc1111188c7cff5419
matsagad <matsagad@gmail.com>
2023-01-28 18:47:30 +0000

    Remove initial state from signature

--- src/ssa/simulator.py
+++ src/ssa/simulator.py
@@ -1,20 +1,21 @@
 from abc import ABC, abstractmethod
 from models.model import PromoterModel
-from numpy import ndarray
+from nptyping import NDArray, Shape, Float
+
 
 class StochasticSimulator(ABC):
     def __init__(self):
         pass
-    
+
     @abstractmethod
-    def simulate(self, model: PromoterModel) -> ndarray:
-      """Simulates the trajectory of the model.
+    def simulate(self, model: PromoterModel) -> NDArray[Shape["Any"], Float]:
+        """Simulates the trajectory of the model.
 
-      Args:
-          model : a promoter model
+        Args:
+            model : a promoter model
 
-      Returns:
-          A time series of the model's state. Its z-dimension is
-          equal to the number of samples from the exogenous input.
-      """
-      pass
+        Returns:
+            A time series of the model's state. Its z-dimension is
+            equal to the number of samples from the exogenous input.
+        """
+        pass
