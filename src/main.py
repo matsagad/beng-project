bfbb6ab093f75255a737579d64b1f51926e5a2d4
matsagad <matsagad@gmail.com>
2023-01-29 14:56:18 +0000

    Allow MI calculation for differentiating between environmental identities

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-import json
-from models.simple import SimpleModel
 from models.model import PromoterModel
 from models.rates.function import RateFunction
+from mi_estimation.decoding import DecodingEstimator
 from utils.data_processing import scaleTSall
-from utils.MIdecodingSimp import estimateMIS
 from ssa.one_step import OneStepSimulator
 from utils.data_processing import scaleTS
 import time
@@ -23,7 +21,7 @@ def import_gluc_data(fname="gluc_data_all.npy", save=True):
                 print(f"{tf} not in ncdata")
 
         min_count = min(ts.shape[0] for ts in full_data)
-        full_data = np.array([ts[:min_count] for ts in full_data])
+        full_data = np.array([[ts[:min_count] for ts in full_data]])
         if save:
             np.save(fname, full_data)
         return full_data
@@ -42,22 +40,33 @@ def import_data(fname="data_all.npy", save=True):
                 print(f"{tf} not in ncdata")
 
         min_count = min(len(stress_test) for ts in full_data for stress_test in ts)
-        full_data = np.array([[stress_test[:min_count] for stress_test in ts] for ts in full_data])
+        full_data = np.moveaxis(
+            np.array(
+                [[stress_test[:min_count] for stress_test in ts] for ts in full_data]
+            ),
+            0,
+            1,
+        )
+
         if save:
             np.save(fname, full_data)
         return full_data
 
-def test_gluc():
+
+def main():
     # Load data
     _, origin, _ = scaleTS("dot6")
 
     ## Batched data (~119 replicates)
-    data = import_gluc_data()
-    print(data.shape)  # num tfs, replicates, time stamps
-    single_data = np.array(data[:, :1])
+    data = import_data()
+    print(data.shape)  # num envs, num tfs, replicates, time stamps
+
+    ## Examples for debugging batching process
+    single_replicate = np.array(data[:, :, :1])
+    single_environment = np.array(data[:1])
 
     # Set-up model
-    tf_index = 1
+    tf_index = 2
     a, b, c = 1.0e-3, 1.0e-2, 1.0e-2
 
     model = PromoterModel(
@@ -73,19 +82,13 @@ def test_gluc():
     sim = OneStepSimulator(data, 2.5, deterministic=False)
     ts = sim.simulate(model)
     print(ts.shape)
-    print(time.time() - start)
-
-    active_state = -1
-    rich = ts[origin - 20 : origin, :, active_state].T
-    stress = ts[origin : origin + 20, :, active_state].T
-    print(f"rich: {np.sum(rich, axis=1)}")
-    print(f"stress: {np.sum(stress, axis=1)}")
+    print(f"Simulation done! ({'%.3f' % (time.time() - start)}s)")
 
     # Evaluate
-    print("calculating")
-    miscore = estimateMIS([[rich, stress]])[0]
-    print(f"MI: {miscore}")
+    est = DecodingEstimator(origin, 20)
+    miscore = est.estimate(model, ts)
+    print(f"MI found: {miscore}")
 
 
 if __name__ == "__main__":
-    test_gluc()
+    main()
