fe3d53adc12939e4bed5ec3ffa5b544ca36bcf1c
matsagad <matsagad@gmail.com>
2023-01-29 18:26:32 +0000

    Adapt to new class structure

--- models/simple.py
+++ models/simple.py
@@ -5,7 +5,7 @@ from models.rates.function import RateFunction
 class SimpleModel(PromoterModel):
     def __init__(self, tf_index: int, a: float, b: float):
         rate_fn_matrix = [
-            [None, RateFunction.linear(a, tf_index)],
-            [RateFunction.constant(b), None],
+            [None, RateFunction.Linear(a, tf_index)],
+            [RateFunction.Constant(b), None],
         ]
         super().__init__(rate_fn_matrix)
