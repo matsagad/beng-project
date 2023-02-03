1f92cd660a38a6f94e4f755d348fb9445bbda4c4
matsagad <matsagad@gmail.com>
2023-02-03 14:30:21 +0000

    Allow plotting of multiple TFs

--- optimisation/grid_search.py
+++ optimisation/grid_search.py
@@ -8,39 +8,49 @@ import time
 
 class GridSearch:
     def optimise(self, exogenous_data):
-        on_count, off_count = 3, 3
+        on_count, off_count = 4, 4
+        fname_temp = f"cache/res_real_tf{{tf_index}}_{on_count}_{off_count}.npy"
         pip_real = OneStepDecodingPipeline(exogenous_data, realised=True)
-        pip_prob = OneStepDecodingPipeline(exogenous_data, realised=False)
 
-        res_real = np.zeros((on_count, off_count))
-        res_prob = np.zeros((on_count, off_count))
-
-        tf_index = 0
+        tf_names = ["msn2", "sfp1", "dot6", "maf1"]
+        num_tfs = exogenous_data.shape[1]  # 4
+        res_real = np.zeros((num_tfs, on_count, off_count))
 
         start = time.time()
         print("0.00%")
 
-        for i, k_on in enumerate(np.logspace(-2, 2, num=on_count)):
-            for j, k_off in enumerate(np.logspace(-2, 2, num=off_count)):
-                model = PromoterModel(
-                    rate_fn_matrix=[
-                        [None, RF.Linear(k_on, tf_index)],
-                        [RF.Constant(k_off), None],
-                    ]
-                )
-                res_real[i, j] = pip_real.evaluate(model)
-                res_prob[i, j] = pip_prob.evaluate(model)
-                print(
-                    f"{'%.2f' % (100 * (i * off_count + j + 1) / (on_count * off_count))}%"
-                )
+        for tf in range(num_tfs):
+            try:
+                res_real[tf] = np.load(fname_temp.format(tf_index=tf))
+                print(f"cached TF{tf} data")
+            except:
+                for i, k_on in enumerate(np.logspace(-2, 2, num=on_count)):
+                    for j, k_off in enumerate(np.logspace(-2, 2, num=off_count)):
+                        model = PromoterModel(
+                            rate_fn_matrix=[
+                                [None, RF.Linear(k_on, tf)],
+                                [RF.Constant(k_off), None],
+                            ]
+                        )
+                        res_real[i, j] = pip_real.evaluate(model)
+                        print(
+                            f"{'%.2f' % (100 * (i * off_count + j + 1) / (on_count * off_count))}%"
+                        )
+            print(f"{tf}/{num_tfs} TFs done.")
 
         print(f"{time.time() - start}s elapsed")
 
-        np.save(f"cache/res_real_tf{tf_index}_{on_count}_{off_count}.npy", res_real)
-        np.save(f"cache/res_prob_tf{tf_index}_{on_count}_{off_count}.npy", res_prob)
+        for tf in range(num_tfs):
+            np.save(fname_temp.format(tf_index=tf), res_real[tf])
 
-        fig, axes = plt.subplots(1, 2, sharey=True)
-        for ax, res in zip(axes, (res_real, res_prob)):
+        fig, axes = plt.subplots(1, num_tfs, sharey=True)
+
+        if num_tfs == 1:
+            axes = [axes]
+
+        for i, pair in enumerate(zip(axes, (res_real))):
+            ax, res = pair
+            ax.title.set_text(tf_names[i])
             im = ax.imshow(
                 res,
                 cmap="magma",
@@ -53,4 +63,4 @@ class GridSearch:
 
         fig.colorbar(im, ax=axes, location="bottom")
 
-        plt.savefig(f"cache/grid_tf{tf_index}_{on_count}_{off_count}", dpi=100)
+        plt.savefig(f"cache/grid_tfs_{on_count}_{off_count}", dpi=100)
