307762863bd0684163b1fc5bf548c351ebc1f933
matsagad <matsagad@gmail.com>
2023-02-04 11:43:57 +0000

    Organise typical models considered

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from models.rates.function import RateFunction as RF
 from optimisation.grid_search import GridSearch
 from pipeline.one_step_decoding import OneStepDecodingPipeline
 from ssa.one_step import OneStepSimulator
+from mi_estimation.decoding import DecodingEstimator
 from utils.data_processing import scaleTSall
 from utils.data_processing import scaleTS
 import numpy as np
@@ -55,6 +56,38 @@ def import_data(fname="cache/data_all.npy", save=True):
 
 
 class Examples:
+    tf_index = 0
+    a, b, c = 1.0e-2, 1.0e-2, 1.0e-2
+    models = {
+        2: PromoterModel(
+            rate_fn_matrix=[[None, RF.Linear(a, tf_index)], [RF.Constant(b), None]]
+        ).with_active_states([1]),
+        3: PromoterModel(
+            rate_fn_matrix=[
+                [None, None, RF.Constant(a)],
+                [None, None, RF.Constant(b)],
+                [
+                    RF.Linear(a, tf_index),
+                    RF.Linear(b, tf_index + 1),
+                    None,
+                ],
+            ]
+        ).with_active_states([0, 1]),
+        4: PromoterModel(
+            rate_fn_matrix=[
+                [None, None, None, RF.Constant(a)],
+                [None, None, None, RF.Constant(b)],
+                [None, None, None, RF.Constant(c)],
+                [
+                    RF.Linear(a, tf_index),
+                    RF.Linear(b, tf_index + 1),
+                    RF.Linear(c, tf_index + 2),
+                    None,
+                ],
+            ]
+        ).with_active_states([0, 1, 2]),
+    }
+
     class UsingThePipeline:
         def pipeline_example():
             # Import data
@@ -67,22 +100,7 @@ class Examples:
             single_environment = np.array(data[:1])
 
             # Set-up model
-            tf_index = 0
-            a, b, c = 1.0e-2, 1.0e-2, 1.0e-2
-
-            model = PromoterModel(
-                rate_fn_matrix=[
-                    [None, None, None, RF.Constant(a)],
-                    [None, None, None, RF.Constant(b)],
-                    [None, None, None, RF.Constant(c)],
-                    [
-                        RF.Linear(a, tf_index),
-                        RF.Linear(b, tf_index + 1),
-                        RF.Linear(c, tf_index + 2),
-                        None,
-                    ],
-                ]
-            ).with_active_states([0, 1, 2])
+            model = Examples.models[3]
 
             # Simulate and evaluate
             pipeline = OneStepDecodingPipeline(data)
@@ -94,6 +112,7 @@ class Examples:
             tf_index = 0
             a, b, c = 1.0e-3, 1.0e-2, 1.0e-2
 
+            # Changes in matrix will reflect in visualisation!
             model = PromoterModel(
                 rate_fn_matrix=[
                     [None, None, None, RF.Constant(a)],
@@ -112,26 +131,8 @@ class Examples:
             model.visualise()
 
         def visualise_trajectory_example():
-            # Import data
             data = import_data()
-
-            # Set-up model
-            tf_index = 0
-            a, b, c = 1.0e-3, 1.0e-2, 1.0e-2
-
-            model = PromoterModel(
-                rate_fn_matrix=[
-                    [None, None, None, RF.Constant(a)],
-                    [None, None, None, RF.Constant(b)],
-                    [None, None, None, RF.Constant(c)],
-                    [
-                        RF.Linear(a, tf_index),
-                        RF.Linear(b, tf_index + 1),
-                        RF.Linear(c, tf_index + 2),
-                        None,
-                    ],
-                ]
-            ).with_active_states([0, 1, 2])
+            model = Examples.models[4]
 
             # Simulate
             sim = OneStepSimulator(data, tau=2.5, realised=True)
@@ -151,16 +152,8 @@ class Examples:
             )
 
         def visualise_realised_probabilistic_trajectories():
-            # Import data
             data = import_data()
-
-            # Set-up models
-            tf_index = 0
-            a, b = 1.0e-2, 1.0e-2
-            rate_fn_matrix = [[None, RF.Linear(a, tf_index)], [RF.Constant(b), None]]
-
-            ## Initial state: [0.5, 0.5]
-            model = PromoterModel(rate_fn_matrix).with_active_states([1])
+            model = Examples.models[2]
 
             print("Realised (initial: [0.5, 0.5])")
             OneStepSimulator.visualise_trajectory(
@@ -178,69 +171,43 @@ class Examples:
 
     class Benchmarking:
         def matrix_exponentials():
-            # Import data
             data = import_data()
-
-            # Set-up model
-            tf_index = 0
-            a, b = 1.01e-2, 1.0e-2
-
-            model = PromoterModel(
-                rate_fn_matrix=[[None, RF.Linear(a, tf_index)], [RF.Constant(b), None]]
-            ).with_active_states([1])
-            print(data.shape)
+            model = Examples.models[2]
 
             start = time.time()
             mat = model.get_matrix_exp(data, 2.5)
             print(time.time() - start)
 
         def trajectory():
-            # Import data
             data = import_data()
-
-            # Set-up model
-            tf_index = 0
-            a, b, c = 1.0e-2, 1.0e-2, 1.0e-2
-
-            ## Initial state: [1, 0]
-            model = PromoterModel(
-                rate_fn_matrix=[[None, RF.Linear(a, tf_index)], [RF.Constant(b), None]]
-            ).with_active_states([1])
-
-            # model = PromoterModel(
-            #     rate_fn_matrix=[
-            #         [None, None, RF.Constant(a)],
-            #         [None, None, RF.Constant(b)],
-            #         [
-            #             RF.Linear(a, tf_index),
-            #             RF.Linear(b, tf_index + 1),
-            #             None,
-            #         ],
-            #     ]
-            # ).with_active_states([0, 1])
-
-            # model = (
-            #     PromoterModel(
-            #         rate_fn_matrix=[
-            #             [None, None, None, RF.Constant(a)],
-            #             [None, None, None, RF.Constant(b)],
-            #             [None, None, None, RF.Constant(c)],
-            #             [
-            #                 RF.Linear(a, tf_index),
-            #                 RF.Linear(b, tf_index + 1),
-            #                 RF.Linear(c, tf_index + 2),
-            #                 None,
-            #             ],
-            #         ]
-            #     ).with_active_states([0, 1, 2])
-            #     # .with_realised_init_state([0.25, 0.25, 0.25, 0.25])
-            # )
+            model = Examples.models[2]
 
             # Simulate
             start = time.time()
+
             sim = OneStepSimulator(data, tau=2.5, realised=False)
             trajectories = sim.simulate(model)
+
             print(time.time() - start)
+
+        def mi_estimation():
+            data = import_data()
+            model = Examples.models[2]
+
+            # Simulate
+            sim = OneStepSimulator(data, tau=2.5, realised=False, replicates=1)
+            trajectories = sim.simulate(model)
+
+            # Estimate MI
+            origin = OneStepDecodingPipeline.FIXED_ORIGIN
+            interval = OneStepDecodingPipeline.FIXED_INTERVAL
+            est = DecodingEstimator(origin, interval)
+
+            print("Estimating MI...")
+            start = time.time()
+            mi_score = est.estimate(model, trajectories)
+            print(time.time() - start)
+            print(f"MI: {mi_score}")
 
     class Optimisation:
         def grid_search():
@@ -251,11 +218,12 @@ class Examples:
 
 def main():
     # Examples.Benchmarking.trajectory()
+    Examples.Benchmarking.mi_estimation()
     # Examples.PlottingVisuals.visualise_model_example()
     # Examples.PlottingVisuals.visualise_trajectory_example()
     # Examples.PlottingVisuals.visualise_realised_probabilistic_trajectories()
     # Examples.UsingThePipeline.pipeline_example()
-    Examples.Optimisation.grid_search()
+    # Examples.Optimisation.grid_search()
 
 
 if __name__ == "__main__":
