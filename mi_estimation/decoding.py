31b56c7c99aa43bd6e94c97d02518f69af47f763
matsagad <matsagad@gmail.com>
2023-02-11 17:48:24 +0000

    Add flag for removing duplicate samples

--- mi_estimation/decoding.py
+++ mi_estimation/decoding.py
@@ -10,7 +10,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
 from sklearn.experimental import enable_halving_search_cv
-from sklearn.model_selection import HalvingGridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, HalvingGridSearchCV, train_test_split
 from typing import Any, Dict
 import numpy as np
 
@@ -44,7 +44,7 @@ class DecodingEstimator(MIEstimator):
                 "max_depth": [16, 64, 128],
                 "min_samples_leaf": [1, 2, 4],
                 "min_samples_split": [2, 4, 8],
-                "n_estimators": [20],
+                "n_estimators": [10],
             },
         },
         "naive_bayes": {
@@ -78,13 +78,13 @@ class DecodingEstimator(MIEstimator):
         cfg = self.default_classifier_cfgs[classifier_name]
         self.classifier = cfg["classifier"]
         self.classifier_params = cfg["params"]
-        print(self.classifier)
         print(classifier_name)
 
     def _split_classes(
         self,
         model: PromoterModel,
         trajectory: NDArray[Shape["Any, Any, Any, Any"], Float],
+        remove_duplicates: bool = False,
     ) -> NDArray[Shape["Any, Any, Any"], Float]:
         CLASS_AXIS = 1
         trajectory = np.moveaxis(trajectory, CLASS_AXIS, 0)
@@ -105,11 +105,26 @@ class DecodingEstimator(MIEstimator):
                 env_class[self.origin : self.origin + self.interval, :, active_states],
                 axis=2,
             ).T
-            states.append(stress_trajectory)
+            states.append(
+                np.unique(stress_trajectory, axis=0)
+                if remove_duplicates
+                else stress_trajectory
+            )
 
-        rich_states = np.array(rich_states)
-        np.random.shuffle(rich_states)
-        return np.array([rich_states[: len(states[0])]] + states)
+        if remove_duplicates:
+            rich_states = np.unique(rich_states, axis=0)
+            samples_per_class = min(len(state) for state in states + [rich_states])
+            for i, state in enumerate(states):
+                states[i] = state[:samples_per_class]
+        else:
+            rich_states = np.array(rich_states)
+            samples_per_class = len(states[0])
+
+        rich_samples = rich_states[
+            np.random.choice(len(rich_states), samples_per_class, replace=False)
+        ]
+
+        return np.array([rich_samples] + states)
 
     def _flatten(self, X):
         return X.reshape((X.shape[0], -1))
@@ -119,7 +134,7 @@ class DecodingEstimator(MIEstimator):
         data: NDArray[Shape["Any, Any, Any"], Float],
         n_bootstraps: int = 25,
         c_interval: int = [0.25, 0.75],
-        verbose: bool = True,
+        verbose: bool = False,
     ) -> float:
         """
         The MI estimation process is adapted from the method of Granados, Pietsch, et al,
@@ -131,7 +146,7 @@ class DecodingEstimator(MIEstimator):
         Xs, ys = np.vstack(data), np.repeat(np.arange(num_classes), num_samples)
 
         ## Split data into validation/training+testing ~ 20/60+20 split
-        data_split = (0.20, 0.50, 0.30)
+        data_split = (0.20, 0.60, 0.20)
 
         fst_split = data_split[0]
         snd_split = data_split[2] / (data_split[1] + data_split[2])
@@ -157,7 +172,12 @@ class DecodingEstimator(MIEstimator):
         )
 
         ## Grid search
-        grid_pipeline = HalvingGridSearchCV(pipe, params, n_jobs=-1, cv=5)
+        grid_pipeline = HalvingGridSearchCV(
+            pipe,
+            params,
+            n_jobs=-1,
+            cv=5,
+        )
         grid_pipeline.fit(X_val, y_val)
         if verbose:
             print(grid_pipeline.best_estimator_)
