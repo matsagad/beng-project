542cc5064777324793506f54d96525eba8933650
matsagad <matsagad@gmail.com>
2023-02-20 23:26:13 +0000

    Add common method for accessing number of parameters

--- models/rates/function.py
+++ models/rates/function.py
@@ -1,5 +1,5 @@
-from typing import List
-from abc import ABC, abstractmethod
+from typing import List, Tuple
+from abc import ABC, abstractmethod, abstractproperty
 from nptyping import NDArray
 import numpy as np
 
@@ -18,19 +18,30 @@ class RateFunction:
         def str(self) -> str:
             pass
 
+        @staticmethod
+        @abstractmethod
+        def num_params() -> Tuple[int, int]:
+            pass
+
     class Constant(Function):
         def evaluate(self, exo_states: NDArray) -> float:
             return self.rates[0] + np.zeros(exo_states.shape[1:])
 
         def str(self) -> str:
-            return f"${self.rates[0]}$"
+            return f"${'{:.3f}'.format(self.rates[0])}$"
+
+        def num_params() -> Tuple[int, int]:
+            return 1, 0
 
     class Linear(Function):
         def evaluate(self, exo_states: NDArray) -> float:
             return self.rates[0] * exo_states[self.tfs[0]]
 
         def str(self) -> str:
-            return f"${self.rates[0]} \cdot TF_{{{self.tfs[0]}}}$"
+            return f"${'{:.3f}'.format(self.rates[0])} \cdot TF_{{{self.tfs[0]}}}$"
+
+        def num_params() -> Tuple[int, int]:
+            return 1, 1
 
     class Hill(Function):
         def evaluate(self, exo_states: NDArray) -> float:
@@ -39,4 +50,10 @@ class RateFunction:
             )
 
         def str(self) -> str:
-            return f"$\\frac{{{self.rates[0]} \cdot TF_{{{self.tfs[0]}}}}}{{{self.rates[1]} + TF_{{{self.tfs[0]}}}}}$"
+            return (
+                f"$\\frac{{{'{:.3f}'.format(self.rates[0])} \cdot TF_{{{self.tfs[0]}}}}}"
+                + "{{{'{:.3f}'.format(self.rates[1])} + TF_{{{self.tfs[0]}}}}}$"
+            )
+
+        def num_params() -> Tuple[int, int]:
+            return 2, 1
