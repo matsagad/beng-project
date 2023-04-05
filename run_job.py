248fe7cbd8b10596c8ad2bb342c346bd8be6e9c2
matsagad <matsagad@gmail.com>
2023-04-05 15:46:17 +0100

    Set internal alarm for when results should be cached

--- run_job.py
+++ run_job.py
@@ -1,11 +1,18 @@
 from jobs.routine_jobs import GeneticAlgorithmJob, ParticleSwarmWeightOptimisationJob
 import sys
+import signal
 
+HOURS = 3600
 jobs = {
     "genetic_algorithm": GeneticAlgorithmJob(verbose=True),
     "pso_weight_optimisation": ParticleSwarmWeightOptimisationJob(verbose=True),
 }
 
+class Alarm(Exception):
+    pass
+
+def alarm_handler(*args):
+    raise Alarm
 
 def main():
     _args = sys.argv[1:]
@@ -21,8 +28,17 @@ def main():
         return
 
     arg_dict = arg_dict = dict(arg.split("=") for arg in args if "=" in arg)
-    jobs[job_type].run(arg_dict)
 
+    max_runtime = int(arg_dict.get("max_runtime", 2 * HOURS))
+
+    signal.signal(signal.SIGALRM, alarm_handler)
+    signal.alarm(max_runtime)
+
+    try:
+        jobs[job_type].run(arg_dict)
+        signal.alarm(0)
+    except Alarm:
+        jobs[job_type].on_interrupted()
 
 if __name__ == "__main__":
     main()
