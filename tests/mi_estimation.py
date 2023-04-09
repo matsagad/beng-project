5c0406e69c7b29ed8783405949442852df6ac059
matsagad <matsagad@gmail.com>
2023-04-09 13:47:44 +0100

    Add test for handling low variance models

--- tests/mi_estimation.py
+++ tests/mi_estimation.py
@@ -1,6 +1,6 @@
-from models.model import PromoterModel
-from models.preset import Preset
 from mi_estimation.decoding import DecodingEstimator
+from models.generator import ModelGenerator
+from models.model import PromoterModel
 from pipeline.one_step_decoding import OneStepDecodingPipeline
 from tests.test import TestWithData
 import numpy as np
@@ -15,16 +15,11 @@ class MIEstimationTests(TestWithData):
         Tests if a model that theoretically outputs a perfectly distinguishable trajectory
         in all environments should attain the maximum MI of 2.0.
         """
-        classifier = "naive_bayes"
-        replicates = 10
-
-        est = DecodingEstimator(
-            self.origin, self.interval, classifier, replicates=replicates
-        )
+        est = DecodingEstimator(**self.default_est_args)
         est.parallel = True
 
         num_envs, _, num_cells, num_times = self.data.shape
-        trajectory = np.zeros((num_times, num_envs, num_cells * replicates, 2))
+        trajectory = np.zeros((num_times, num_envs, num_cells * est.replicates, 2))
 
         # Rich state left as is. Note: first state is active state
         origin = self.origin
@@ -36,3 +31,28 @@ class MIEstimationTests(TestWithData):
         mi_score = est.estimate(model, trajectory)
 
         assert mi_score == 2.0, "Projected MI != 2.0"
+
+    def test_random_model_variance(self):
+        """
+        Tests if low MI models are handled correctly, e.g. there are
+        no sklearn exceptions raised due to zero variance.
+
+        Due to the curse of dimensionality, randomly generated
+        large models will tend to perform poorly and are a good test sets.
+        """
+        threshold = 0.1
+        num_models = 10
+        num_trials = 10
+
+        pip = OneStepDecodingPipeline(**self.default_pip_args)
+        pip.set_parallel()
+
+        for _ in range(num_models):
+            model = ModelGenerator.get_random_model(10)
+            trajectories = pip.simulator.simulate(model)
+            mi_score = pip.estimator.estimate(model, trajectories)
+
+            # If close to boundary, then repeat to ensure no errors occur
+            if mi_score < threshold:
+                for _ in range(num_trials):
+                    mi_score = pip.estimator.estimate(model, trajectories)
