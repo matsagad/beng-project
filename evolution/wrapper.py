46f36504a387ec08d2338e6c46b2d414dd717f69
matsagad <matsagad@gmail.com>
2023-04-15 17:57:47 +0100

    Add methods for novelty search

--- evolution/wrapper.py
+++ evolution/wrapper.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from models.model import PromoterModel
 from nptyping import NDArray
+import numpy as np
 
 
 class ModelWrapper:
@@ -10,16 +11,29 @@ class ModelWrapper:
         fitness: float = 0,
         mi: float = 0,
         runs_left: int = 3,
+        novelty: float = 0,
         classes: NDArray = None,
     ):
         self.model = model
         self.fitness = fitness
         self.mi = mi
         self.runs_left = runs_left
+        self.novelty = novelty
         self.classes = classes
 
     def __lt__(self, other: "ModelWrapper") -> bool:
+        """
+        Used for heapq sort during genetic algorithm.
+        """
         return -self.fitness < -other.fitness
 
     def as_tuple(self) -> Tuple[float, float, int, PromoterModel]:
         return self.fitness, self.mi, self.runs_left, self.model
+
+    def as_nn_array(self) -> NDArray:
+        return self.classes.flatten()
+
+    def dominates(self, other: "ModelWrapper") -> bool:
+        not_worse = self.novelty >= other.novelty and self.local_fitness >= other.local_fitness
+        is_better = self.novelty > other.novelty or self.local_fitness > other.local_fitness
+        return not_worse and is_better
