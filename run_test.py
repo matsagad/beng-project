2d0600e624ab2497463b7036772c14873f3d2293
matsagad <matsagad@gmail.com>
2023-04-21 16:36:43 +0100

    Verify topology measure is a distance metric

--- run_test.py
+++ run_test.py
@@ -1,9 +1,12 @@
-from tests.mi_estimation import MIEstimationTests
+from tests.distance_metric import DistanceMetricTests
 from tests.genetic_operator import GeneticOperatorTests
+from tests.mi_estimation import MIEstimationTests
 
 def main():
-  MIEstimationTests().run_all_tests()
+  DistanceMetricTests().run_all_tests()
   GeneticOperatorTests().run_all_tests()
+  MIEstimationTests().run_all_tests()
+
 
 
 if __name__ == "__main__":
--- tests/distance_metric.py
+++ tests/distance_metric.py
@@ -8,7 +8,7 @@ class DistanceMetricTests(TestWithData):
     def __init__(self):
         super().__init__()
 
-    def trajectory_measure_is_a_metric(self):
+    def test_trajectory_measure_is_a_metric(self):
         from evolution.novelty.metrics import TrajectoryMetric
         from itertools import permutations
 
@@ -27,17 +27,42 @@ class DistanceMetricTests(TestWithData):
         multi_trajectories = []
         avg_trajectories = []
 
-        print("Finding trajectories...")
+        print("\tFinding trajectories...")
         for model in models:
             multi_trajectories.append(get_classes(model))
             avg_trajectories.append(np.average(multi_trajectories[-1], axis=1))
 
         d = TrajectoryMetric.rms_js_metric_for_trajectories
 
-        print("Checking...")
+        print("\tChecking...")
         for trajectories in (multi_trajectories, avg_trajectories):
             for i, j, k in permutations(trajectories, 3):
                 if d(i, k) > d(i, j) + d(j, k):
                     assert (
                         False
                     ), f"Triangle inequality condition failed: d(i, j) + d(j, k) < d(i, k), {d(i, j) + d(j, k):.3f} < {d(i, k):.3f}"
+
+    def test_topology_measure_is_a_metric(self):
+        from evolution.novelty.metrics import TopologyMetric
+        from itertools import permutations
+
+        n_models = 50
+
+        models = [
+            ModelGenerator.get_random_model(states)
+            for states in np.random.choice(4, n_models) + 2
+        ]
+
+        feature_vectors = [TopologyMetric.get_feature_vector(model) for model in models]
+
+        d = TopologyMetric.wwl_metric_for_wl_feature_vectors
+
+        print("\tChecking...")
+        for i, j, k in permutations(feature_vectors, 3):
+            # Round calculations to avoid float imprecision leading to
+            # incorrect comparison. This happens as the topology metric
+            # often calculates the same distance, e.g. 0.87566666666 and 0.7566666667
+            if round(d(i, k), 8) > round(d(i, j) + d(j, k), 8):
+                assert (
+                    False
+                ), f"Triangle inequality condition failed: d(i, j) + d(j, k) <= d(i, k), {d(i, j) + d(j, k):.8f} < {d(i, k):.8f}"
